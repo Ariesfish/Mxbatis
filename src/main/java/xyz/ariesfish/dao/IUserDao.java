339dbf0571d122f4359341e34ba6ee1388ab307d
Ariesfish <zzlyldy@gmail.com>
2020-01-28 21:03:07 +0800

    change to use annotation

--- src/main/java/xyz/ariesfish/dao/IUserDao.java
+++ src/main/java/xyz/ariesfish/dao/IUserDao.java
@@ -1,10 +1,12 @@
 package xyz.ariesfish.dao;
 
+import org.apache.ibatis.annotations.Select;
 import xyz.ariesfish.domain.User;
 
 import java.util.List;
 
 public interface IUserDao {
 
+    @Select("select * from user")
     List<User> findAll();
 }
